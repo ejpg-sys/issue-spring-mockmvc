ea844a37ba8d3ee70487a250cfd2400a10793303
Eduardo Junior Pereira Garcia <eduardojuniorpereiragarcia@gmail.com>
2024-11-14 15:15:39 -0300

    ISSUE 33884
    https://github.com/spring-projects/spring-framework/issues/33884
    
    This commit add implementation on ControllerTest using MockMvc of
    Spring Ecossystem, all related implementation are the same as
    described in approval using cURL related on topic 1 of README.md
    on root of this project. The expectations using this framework
    was not archived because the behavior was different from the
    realtime response execution using cURL. The analysis behavior
    of application design are related on README.md topic 2.

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ Copyright (c) 2024 Eduardo Junior Pereira Garcia
 https://github.com/ejpg-sys/issue-spring-mockmvc  
 
 
-Step by Step
+# Step by Step
+
+# 1. Requests to Spring Boot WebService with cURL
 
 1.1.: Request GET /hello **without** SESSION-KEY using cURL
 > curl http://localhost:33884/hello  
@@ -46,3 +48,36 @@ Step by Step
   --data '{"number": "33885"}'  
 >> response: {"status":401,"message":"401 UNAUTHORIZED"}  
 [BDD APPROVED]  
+
+
+# 2. Requests to Spring Boot WebService with MockMvc
+
+2.1.: Request GET /hello **without** SESSION-KEY using MockMvc  
+> doHelloWithoutSessionKeyTest  
+>> something is wrong: status expected 401 but was 200  
+[BDD REPROVED]  
+
+2.2.: Request GET /hello **with wrong** SESSION-KEY using MockMvc  
+> doHelloWithWrongSessionKeyTest  
+>> something is wrong: status expected 401 but was 200  
+[BDD REPROVED]  
+
+2.3.: Request GET /hello **with** SESSION-KEY using MockMvc  
+> doHelloWithRightSessionKey  
+>> it's all right: status expected 200 and response was 200  
+[BDD APPROVED]  
+
+2.4.: Request POST /authorization **without** SESSION-KEY using MockMvc  
+> doAuthorizationWithoutSessionKey  
+>> something is wrong: status expected 401 but was 202  
+[BDD REPROVED]  
+
+2.5.: Request POST /authorization **with** SESSION-KEY using MockMvc  
+> doAuthorizationWithRightSessionKey  
+>> it's all right:  status expected 202 and response was 202  
+[BDD APPROVED]  
+
+2.6.: Request POST /authorization **with** SESSION-KEY **and wrong** GiftCard using MockMvc  
+> doAuthorizationWithRightSessionKeyAndWrongGiftCardNumber  
+>> it's all right: status expected 401 and response was 401  
+[BDD APPROVED]   
--- src/test/java/ejpg/issue/spring/mockmvc/HelloControllerTest.java
+++ src/test/java/ejpg/issue/spring/mockmvc/HelloControllerTest.java
@@ -6,44 +6,80 @@
  */
 package ejpg.issue.spring.mockmvc;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import ejpg.issue.spring.mockmvc.dto.GiftCardDTO;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class HelloControllerTest {
 
 	@Test
-	void doHelloWithoutSessionKeyTest() {
-		fail("Not yet implemented!");
+	void doHelloWithoutSessionKeyTest(@Autowired MockMvc mvc) throws Exception {
+		mvc.perform(get("/hello"))
+		.andExpect(status().isUnauthorized());
 	}
 
 	@Test
-	void doHelloWithWrongSessionKeyTest() {
-		fail("Not yet implemented!");
+	void doHelloWithWrongSessionKeyTest(@Autowired MockMvc mvc) throws Exception {
+		mvc.perform(get("/hello")
+				.header("SESSION-KEY", "33885"))
+		.andExpect(status().isUnauthorized());
 	}
 
 	@Test
-	void doHelloWithRightSessionKey() {
-		fail("Not yet implemented!");
+	void doHelloWithRightSessionKey(@Autowired MockMvc mvc) throws Exception {
+		mvc.perform(get("/hello")
+				.header("SESSION-KEY", "33884"))
+		.andExpect(status().isOk());
 	}
 
 	@Test
-	void doAuthorizationWithoutSessionKey() {
-		fail("Not yet implemented!");
+	void doAuthorizationWithoutSessionKey(@Autowired MockMvc mvc) throws Exception {
+		GiftCardDTO data = new GiftCardDTO();
+		data.setNumber(33884);
+		byte[] content = new ObjectMapper().writeValueAsBytes(data);
+		
+		mvc.perform(post("/authorized")
+				.contentType("application/json")
+				.content(content))
+		.andExpect(status().isUnauthorized());
 	}
 
 	@Test
-	void doAuthorizationWithRightSessionKey() {
-		fail("Not yet implemented!");
+	void doAuthorizationWithRightSessionKey(@Autowired MockMvc mvc) throws Exception {
+		GiftCardDTO data = new GiftCardDTO();
+		data.setNumber(33884);
+		byte[] content = new ObjectMapper().writeValueAsBytes(data);
+		
+		mvc.perform(post("/authorized")
+				.header("SESSION-KEY", "33884")
+				.contentType("application/json")
+				.content(content))
+		.andExpect(status().isAccepted());
 	}
 
 	@Test
-	void doAuthorizationWithRightSessionKeyAndWrongGiftCardNumber() {
-		fail("Not yet implemented!");
+	void doAuthorizationWithRightSessionKeyAndWrongGiftCardNumber(@Autowired MockMvc mvc) throws Exception {
+		GiftCardDTO data = new GiftCardDTO();
+		data.setNumber(33885);
+		byte[] content = new ObjectMapper().writeValueAsBytes(data);
+		
+		mvc.perform(post("/authorized")
+				.header("SESSION-KEY", "33885")
+				.contentType("application/json")
+				.content(content))
+		.andExpect(status().isUnauthorized());
 	}
 
 }
